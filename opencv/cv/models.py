4545fe96525a6e7a3e84e9057eebdb65ebcaf81d
Md Sabbir Rahman <56318666+Sabbir-Rahman@users.noreply.github.com>
2021-01-16 01:55:58 +0600

    get_filtered_image function on going

--- opencv/cv/models.py
+++ opencv/cv/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+from .utils import get_filtered_image
+from PIL import Image
+import numpy as np
+
+
 
 # Create your models here.
 
@@ -19,4 +24,15 @@ class Upload(models.Model):
     action = models.CharField(max_length=50, choices=ACTION_CHOICES)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
-    
+
+    def __str__(self):
+        return str(self.id)
+
+    def save(self, *args, **kwargs):
+
+        #open image
+        pil_img = Image.open(self)
+
+        #convert the image to array and do some processing
+        cv_img = np.array(pil_img)
+        img = get_filtered_image(cv_img, self.action)
