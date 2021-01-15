2d6ddd5c9f014355882f3abd2b64d2b2937c6418
Md Sabbir Rahman <56318666+Sabbir-Rahman@users.noreply.github.com>
2021-01-16 01:23:27 +0600

    model on going

--- opencv/cv/models.py
+++ opencv/cv/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+# some tuples
+ACTION_CHOICES = (
+    ('NO_FILTER', 'no filter'),
+    ('COLORIZED', 'colorized'),
+    ('GRAYSCALE', 'grayscale'),
+    ('BLURRED', 'blurred'),
+    ('BINARY', 'binary'),
+    ('INVERT', 'invert'),
+)
+
+
+
+class Upload(models.Model):
+    image = models.ImageField(upload_to='images')
+    action = models.CharField(max_length=50, choices=ACTION_CHOICES)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    
