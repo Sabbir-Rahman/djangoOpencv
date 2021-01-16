b597fe53279955a7a815e3f0a6b354a8f2a44c50
Md Sabbir Rahman <56318666+Sabbir-Rahman@users.noreply.github.com>
2021-01-16 12:49:32 +0600

    primary target of using filter with the help of django open cv. The out puts are available in media/images folder

--- opencv/cv/models.py
+++ opencv/cv/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from .utils import get_filtered_image
 from PIL import Image
 import numpy as np
-
-
+from io import BytesIO
+from django.core.files.base import ContentFile
 
 # Create your models here.
 
@@ -31,8 +31,24 @@ class Upload(models.Model):
     def save(self, *args, **kwargs):
 
         #open image
-        pil_img = Image.open(self)
+        pil_img = Image.open(self.image)
 
         #convert the image to array and do some processing
         cv_img = np.array(pil_img)
         img = get_filtered_image(cv_img, self.action)
+
+        #convert back to pil image
+        im_pil = Image.fromarray(img)
+
+        #save
+        buffer = BytesIO()
+        im_pil.save(buffer, format='png')
+
+        image_png = buffer.getvalue()
+
+        self.image.save(str(self.image), ContentFile(image_png), save= False)
+
+        super().save(*args, **kwargs)
+
+
+
--- opencv/cv/utils.py
+++ opencv/cv/utils.py
@@ -1,9 +1,10 @@
 import cv2
 
-def get_filtered_image( image, action):
+
+def get_filtered_image(image, action):
     img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     if action == 'NO_FILTER':
-        filtered = img
+        filtered = image
     elif action == 'COLORIZED':
         filtered = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     elif action == 'GRAYSCALE':
@@ -12,11 +13,19 @@ def get_filtered_image( image, action):
         width, height = img.shape[:2]
         if width > 500:
             k = (50, 50)
-        elif width > 200 and width <=500:
-            k = (25,25)
+        elif width > 200 and width <= 500:
+            k = (25, 25)
         else:
-            k = (10,10)
+            k = (10, 10)
+
         blur = cv2.blur(img, k)
         filtered = cv2.cvtColor(blur, cv2.COLOR_BGR2RGB)
+    elif action == 'BINARY':
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        _, filtered = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY)
+    elif action == 'INVERT':
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        _, img = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY)
+        filtered = cv2.bitwise_not(img)
 
-    return filtered;
+    return filtered
